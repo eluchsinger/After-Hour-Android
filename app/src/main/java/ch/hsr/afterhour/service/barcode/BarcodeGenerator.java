5904b391b12df93bb8a233747830a3298d7caf92
Esteban Luchsinger <eluchsingerm@hotmail.com>
2017-04-27 21:44:06 +0200

    Added own barcode interface AH-65

---
+++ app/src/main/java/ch/hsr/afterhour/service/barcode/BarcodeGenerator.java
@@ -0,0 +1,27 @@
+package ch.hsr.afterhour.service.barcode;
+
+import android.graphics.Bitmap;
+import android.graphics.Color;
+
+/**
+ * Created by Esteban Luchsinger on 27.04.2017.
+ * Handles the Barcode Generation.
+ */
+public interface BarcodeGenerator {
+    /**
+     * Generate a barcode containing the data provided.
+     * Default colors and imagetypes.
+     * @param data The data string that should be contained in the barcode. This may fail if the data string is too long!
+     * @return Returns a bitmap representing the Barcode.
+     */
+    Bitmap generateBarcode(String data);
+
+    /**
+     * Generates a barcode containing the data provided and utilizing custom colors.
+     * @param data The data string that should be contained in the barcode.  This may fail if the data string is too long!
+     * @param foregroundColor The color of the barcode. Use the {@link Color} class to choose the color.
+     * @param backgroundColor The color of the background. Use the {@link Color} class to choose the color.
+     * @return Returns a bitmap with the desired barcode.
+     */
+    Bitmap generateBarcode(String data, int foregroundColor, int backgroundColor);
+}
