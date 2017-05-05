a6e39045fe8d8a43017bf580ac4e6d969a636753
fdschwyter <fab.schwyter@gmail.com>
2017-05-05 14:52:42 +0200

    Fixed Bug

--- app/src/main/java/ch/hsr/afterhour/gui/EventRecyclerViewAdapter.java
+++ app/src/main/java/ch/hsr/afterhour/gui/EventRecyclerViewAdapter.java
@@ -52,9 +52,6 @@ public class EventRecyclerViewAdapter extends RecyclerView.Adapter<EventRecycler
                 // Notify the active callbacks interface (the activity, if the
                 // fragment is attached to one) that an item has been selected.
                 holder.mEventDetails.setVisibility(View.VISIBLE);
-
-
-                mListener.onMyEventInteraction(holder.mItem);
             }
         });
 
