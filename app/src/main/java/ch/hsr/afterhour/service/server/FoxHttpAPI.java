6ee6160aef110830dd7d68651cb308aa36c84cea
fdschwyter <fab.schwyter@gmail.com>
2017-05-22 16:57:25 +0200

    Fixed Bug

--- app/src/main/java/ch/hsr/afterhour/service/server/FoxHttpAPI.java
+++ app/src/main/java/ch/hsr/afterhour/service/server/FoxHttpAPI.java
@@ -17,7 +17,6 @@ import ch.viascom.groundwork.foxhttp.body.request.FoxHttpRequestBody;
 import ch.viascom.groundwork.foxhttp.body.request.RequestObjectBody;
 import ch.viascom.groundwork.foxhttp.builder.FoxHttpClientBuilder;
 import ch.viascom.groundwork.foxhttp.exception.FoxHttpException;
-import ch.viascom.groundwork.foxhttp.exception.FoxHttpRequestException;
 import ch.viascom.groundwork.foxhttp.interceptor.FoxHttpInterceptorType;
 import ch.viascom.groundwork.foxhttp.log.SystemOutFoxHttpLogger;
 import ch.viascom.groundwork.foxhttp.parser.GsonParser;
@@ -137,5 +136,15 @@ public class FoxHttpAPI {
         foxHttpRequest.setRequestType(RequestType.GET);
         FoxHttpResponse foxHttpResponse = foxHttpRequest.execute();
         return foxHttpResponse.getParsedBody(Ticket.class);
+    }
+
+    public Bitmap getProfileImage(final int userId) throws MalformedURLException, FoxHttpException {
+        FoxHttpRequest foxHttpRequest = new FoxHttpRequest(httpClient);
+        String urlParameters = "/events/" + userId + "/image";
+        foxHttpRequest.setUrl("{host}" + urlParameters);
+        foxHttpRequest.setRequestType(RequestType.GET);
+        FoxHttpResponse foxHttpResponse = foxHttpRequest.execute();
+        final byte[] bytes = foxHttpResponse.getResponseBody().getBody().toByteArray();
+        return BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
     }
 }
---
+++ app/src/main/java/ch/hsr/afterhour/tasks/DownloadProfileImageTask.java
@@ -0,0 +1,40 @@
+package ch.hsr.afterhour.tasks;
+
+import android.graphics.Bitmap;
+import android.os.AsyncTask;
+
+import java.net.MalformedURLException;
+
+import ch.hsr.afterhour.Application;
+import ch.hsr.afterhour.model.User;
+import ch.viascom.groundwork.foxhttp.exception.FoxHttpException;
+
+/**
+ * Created by Fabian on 22.05.17.
+ */
+
+public class DownloadProfileImageTask extends AsyncTask<User, Void, Bitmap>  {
+    private final OnTaskCompleted<Bitmap> onTaskCompleted;
+
+    public DownloadProfileImageTask(final OnTaskCompleted<Bitmap> onTaskCompleted) {
+        this.onTaskCompleted = onTaskCompleted;
+    }
+
+    @Override
+    protected Bitmap doInBackground(User... params) {
+        try {
+            User user = params[0];
+            return Application.get().getServerAPI().getProfileImage(user.getId());
+        } catch (FoxHttpException | MalformedURLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    @Override
+    protected void onPostExecute(Bitmap bitmap) {
+        super.onPostExecute(bitmap);
+
+        this.onTaskCompleted.onTaskCompleted(bitmap);
+    }
+}
