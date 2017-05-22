1dd83aeeb1749dd72e4586857b6ac1dc6bf9014e
Esteban Luchsinger <eluchsingerm@hotmail.com>
2017-05-22 22:29:24 +0200

    Fixed Menu

--- app/src/main/java/ch/hsr/afterhour/gui/MainActivity.java
+++ app/src/main/java/ch/hsr/afterhour/gui/MainActivity.java
@@ -3,27 +3,31 @@ package ch.hsr.afterhour.gui;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.support.design.widget.CoordinatorLayout;
-import android.support.design.widget.Snackbar;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.Fragment;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.view.menu.ActionMenuItemView;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.TableLayout;
-import android.widget.Toast;
+import android.view.View;
 
 import ch.hsr.afterhour.Application;
 import ch.hsr.afterhour.R;
 import ch.hsr.afterhour.gui.adapters.MainActivityViewPagerAdapter;
 import ch.hsr.afterhour.gui.utils.FragmentWithIcon;
+import ch.hsr.afterhour.gui.utils.ReloadableFragment;
 
-public class MainActivity extends AppCompatActivity implements ActivityCompat.OnRequestPermissionsResultCallback {
+public class MainActivity extends AppCompatActivity implements ActivityCompat.OnRequestPermissionsResultCallback, ViewPager.OnPageChangeListener {
 
     private final static String LOGIN_PREFS = "login_credentials";
     private final static String ACTIVITY_TITLE = "After-Hour";
+    private ViewPager viewPager;
+    private MainActivityViewPagerAdapter mainActivityViewPagerAdapter;
+    private Menu menu;
+    private boolean isReloadVisible;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,18 +37,19 @@ public class MainActivity extends AppCompatActivity implements ActivityCompat.On
         final Toolbar myToolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(myToolbar);
 
-        final ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
+        viewPager = (ViewPager) findViewById(R.id.viewpager);
         // This sets the amount of pages that can be off screen without being destroyed.
         // We will set this to 0, in order to avoid having problems with the scanners
         viewPager.setOffscreenPageLimit(0);
         final TabLayout tabLayout = (TabLayout) findViewById(R.id.tablayout);
         boolean isEmployee = Application.get().getUser().isEmployee();
-        final MainActivityViewPagerAdapter pagerAdapter = new MainActivityViewPagerAdapter(this, getSupportFragmentManager(), isEmployee);
-        viewPager.setAdapter(pagerAdapter);
+        mainActivityViewPagerAdapter = new MainActivityViewPagerAdapter(this, getSupportFragmentManager(), isEmployee);
+        viewPager.setAdapter(mainActivityViewPagerAdapter);
+        viewPager.addOnPageChangeListener(this);
         // Select the middle item.
-        viewPager.setCurrentItem(pagerAdapter.getCount()/2);
+        viewPager.setCurrentItem(mainActivityViewPagerAdapter.getCount()/2);
         tabLayout.setupWithViewPager(viewPager);
-        setTabIcons(tabLayout, pagerAdapter);
+        setTabIcons(tabLayout, mainActivityViewPagerAdapter);
     }
 
     /**
@@ -67,12 +72,18 @@ public class MainActivity extends AppCompatActivity implements ActivityCompat.On
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
+        this.menu = menu;
+        MenuItem reloadItem = menu.findItem(R.id.menu_reload);
+        reloadItem.setVisible(this.isReloadVisible);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch(item.getItemId()) {
+            case R.id.menu_reload:
+                reload();
+                break;
             case R.id.menu_logout:
                 logout();
                 break;
@@ -87,5 +98,41 @@ public class MainActivity extends AppCompatActivity implements ActivityCompat.On
         final Intent loginIntent = new Intent(this, LoginActivity.class);
         loginIntent.setFlags(loginIntent.getFlags() | Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
         startActivity(loginIntent);
+    }
+
+    /**
+     * The child fragments reload their data.
+     */
+    public void reload() {
+        int currentPosition = this.viewPager.getCurrentItem();
+        final Fragment fragment = this.mainActivityViewPagerAdapter.getItem(currentPosition);
+        if(fragment instanceof ReloadableFragment) {
+            ((ReloadableFragment)fragment).onReloadRequested();
+        }
+    }
+
+    public void setReloadMenuItemVisibility(boolean visible) {
+        this.isReloadVisible = visible;
+        invalidateOptionsMenu();
+    }
+
+    @Override
+    public void onPageSelected(int position) {
+        final Fragment fragment = mainActivityViewPagerAdapter.getItem(position);
+        if(fragment instanceof ReloadableFragment) {
+            this.setReloadMenuItemVisibility(true);
+        } else {
+            this.setReloadMenuItemVisibility(false);
+        }
+    }
+
+    @Override
+    public void onPageScrollStateChanged(int state) {
+
+    }
+
+    @Override
+    public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
     }
 }
