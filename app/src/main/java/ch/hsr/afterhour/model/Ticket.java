7608c012897c6fc55a0c029a195033cee32a1dfd
Marshalluous <marcel.stocker@hsr.ch>
2017-05-28 11:11:07 +0200

    updated Ticket.class

--- app/src/main/java/ch/hsr/afterhour/model/Ticket.java
+++ app/src/main/java/ch/hsr/afterhour/model/Ticket.java
@@ -6,6 +6,24 @@ public class Ticket implements Serializable {
     private int id;
     private Event event;
     private Enum type;
+    private String soldDate;
+    private TicketCategory ticketCategory;
+
+    public String getSoldDate() {
+        return soldDate;
+    }
+
+    public void setSoldDate(String soldDate) {
+        this.soldDate = soldDate;
+    }
+
+    public TicketCategory getTicketCategory() {
+        return ticketCategory;
+    }
+
+    public void setTicketCategory(TicketCategory ticketCategory) {
+        this.ticketCategory = ticketCategory;
+    }
 
     public Event getEvent() {
         return event;
